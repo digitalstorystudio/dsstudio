953e299e508010c7c1db997d8def484ce9626557
digitalstorystudio <thedigitalstorystudio@gmail.com>
2026-02-18 21:51:48 +0530

    Delete Blog directory

--- Blog/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const supabaseUrl = "https://zkekqsvnbfelsuqkuwoz.supabase.co";
-const supabaseKey = "sb_publishable__cvthxlZb5tbI4UyNXwbkg_bDLjseJ0";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
